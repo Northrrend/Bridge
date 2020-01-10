4b71b555fda6a67d11b406bfe639eab9c885c31f
xjq <xjq_314@163.com>
2020-01-10 11:22:39 +0800

    merge record and replay

--- replay.py
+++ replay.py
@@ -36,8 +36,48 @@ class KeyboardActionExecute(threading.Thread):
                         time.sleep(0.01)
                 line = file.readline()
 
+class KeyboardActionListener(threading.Thread):
+
+    def __init__(self, file_name):
+        super(KeyboardActionListener, self).__init__()
+        self.file_name = file_name
+
+    def run(self):
+        with open(self.file_name, 'w') as file:
+            def on_press(key):
+                template = keyboard_action_template()
+                template['event'] = 'press'
+                try:
+                    template['vk'] = key.vk
+                except AttributeError:
+                    template['vk'] = key.value.vk
+                finally:
+                    file.writelines(json.dumps(template) + "\n")
+                    file.flush()
+
+            def on_release(key):
+                if key == keyboard.Key.esc:
+                    return False
+                template = keyboard_action_template()
+                template['event'] = 'release'
+                try:
+                    template['vk'] = key.vk
+                except AttributeError:
+                    template['vk'] = key.value.vk
+                finally:
+                    file.writelines(json.dumps(template) + "\n")
+                    file.flush()
+
+            with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+                listener.join()
+
 
 def action(filename):
 
     m2 = KeyboardActionExecute(file_name=filename)
     m2.start()
+
+time.sleep(4)
+print 'record start'
+m1 = KeyboardActionListener(file_name='keyboard.action')
+m1.start()
